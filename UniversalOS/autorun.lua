882be7e82d95096062f6852766e9d11636658f47
HeroBrine1st <herobrine1.videos@gmail.com>
2016-11-25 22:08:57 +0300

    Update autorun.lua

--- UniversalOS/autorun.lua
+++ UniversalOS/autorun.lua
@@ -13,11 +13,11 @@ local function centerText(y, text, color)
 
 
 function bootanimation()
-local progressBar11 = "-" 
+local progressBar11 = "." 
 local progressBar12 = " " 
-local progressBar13 = "-"
+local progressBar13 = "."
 local progressBar21 = " "
-local progressBar22 = "-"
+local progressBar22 = "."
 local progressBar23 = " "
 while true do
 centerText(w/2-1,progressBar11,0xFFFFFF)
