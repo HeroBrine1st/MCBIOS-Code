41fe7a426899b8b2f53a9a3784b78c508ff4b298
HeroBrine1st <herobrine1.videos@gmail.com>
2016-11-26 16:35:50 +0300

    Update install.lua

--- UniversalOS/UOS/install.lua
+++ UniversalOS/UOS/install.lua
@@ -1,1 +1,66 @@
+local filesystem = require("filesystem")
+local component = require("component")
+local computer = require("computer")
+local fs = require("filesystem")
+local internet = require("internet")
+local serialization = require("serialization")
+local shell = require("shell")
+local GitHubUrl = "https://raw.githubusercontent.com/"
 
+local function internetRequest(url)
+  local success, response = pcall(component.internet.request, url)
+    if success then
+    local responseData = ""
+    while true do
+      local data, responseChunk = response.read()
+      if data then
+        responseData = responseData .. data
+      else
+        if responseChunk then
+         return false, responseChunk
+        else
+      return true, responseData
+    end
+    end
+    end
+  else
+    return false, reason
+  end
+end
+ local write = io.write
+local read = io.read
+
+
+
+local function getFromGitHub(url,filepath)
+ local success, reason = internetRequest(url)
+ if success then
+   fs.makeDirectory(fs.path(filepath) or "")
+   fs.remove(filepath)
+   local file = io.open(filepath, "w")
+   file:write(reason)
+   file:close()
+   return reason
+ else
+   error("Can't download " .. url)
+ end
+end
+
+print("Downloading file list")
+
+getFromGitHub("https://raw.githubusercontent.com/HeroBrine1st/OpenComputers/master/UniversalOS/UOS/applications.txt","/UOS/applications.txt")
+
+local applications
+local dfile = "return " .. string.gsub(getFromGitHub("https://raw.githubusercontent.com/HeroBrine1st/OpenComputers/master/UniversalOS/UOS/applications.txt","/UOS/applications.txt"),"\n","")
+
+local file = io.open("/UOS/apps.lua","w")
+file:write(dfile)
+file:close()
+applications = dofile("/UOS/apps.lua")
+
+for i = 1, #applications do
+if applications[i].preLoad == false then
+write("Downloading \"" .. applications[i].name .. "\"\n")
+getFromGitHub(applications[i].url, applications[i].path)
+end
+end
