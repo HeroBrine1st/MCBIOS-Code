61fcf7c4ac6f88cb99bf6a770462c017a196863b
HeroBrine1st <herobrine1.videos@gmail.com>
2016-11-26 12:00:19 +0300

    Update autorun.lua

--- UniversalOS/autorun.lua
+++ UniversalOS/autorun.lua
@@ -7,7 +7,8 @@ local function centerText(y, text)
       local x = math.floor(w / 2 - lenght / 2)
       gpu.set(x,y,text)
   end
-
+gpu.setBackground(0x000000)
+gpu.setForeground(0xFFFFFF)
 gpu.fill(1,1,w,h," ")
 centerText(h/2-1,"UniversalOS")
 centerText(h/2,"Booting system")
