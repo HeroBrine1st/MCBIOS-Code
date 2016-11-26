592c8082fae2c22179754b84f097cba7a6d8b01c
HeroBrine1st <herobrine1.videos@gmail.com>
2016-11-26 11:37:23 +0300

    Update autorun.lua

--- UniversalOS/autorun.lua
+++ UniversalOS/autorun.lua
@@ -2,21 +2,19 @@ local component = require("component")
 local gpu = component.gpu
 local unicode = require("unicode")
 local w,h = gpu.getResolution()
-local function centerText(y, text, color)
+local function centerText(y, text)
       local lenght = unicode.len(text)
       local x = math.floor(w / 2 - lenght / 2)
-      local oldcolor = gpu.setForeground(color)
       gpu.set(x,y,text)
-      gpu.setForeground(oldcolor)
   end
 
 gpu.fill(1,1,w,h," ")
-centerText(w/2,"Booting system",0xFFFFFF)
+centerText(w/2,"Booting system")
 
 local function status(msg)
---gpu.setForeground(0x071910)
+gpu.setForeground(0x434343)
 gpu.set(1,h,msg)
---gpu.setForeground(0xFFFFFF)
+gpu.setForeground(0xFFFFFF)
 end
 
 status("Test")
