45b30b3474e1971812a39d079e6f03fec26e813e
HeroBrine1st <herobrine1.videos@gmail.com>
2016-11-29 18:27:58 +0300

    Update install.lua

--- UniversalOS/UOS/install.lua
+++ UniversalOS/UOS/install.lua
@@ -66,29 +66,43 @@ getFromGitHub(applications[i].url, applications[i].path)
 end
 end
 
-write("\nCheck downloaded files...\n")
+write("\nCheck downloaded files...\n\n")
 
-for i = 1, #applications do
-print("Check " .. applications[i].path)
-local size = fs.size(applications[i].path)
-if fs.exists(applications[i].path) == true then
-  local file = io.open(applications[i].path,"r")
-  local text = file:read(size+1)
+if touch[4]==2 then
+  print("Downloading file list...")
+  local applications
+  local dfile = "return " .. string.gsub(getFromGitHub("https://raw.githubusercontent.com/HeroBrine1st/OpenComputers/master/UniversalOS/UOS/applications.txt","/UOS/applications.txt"),"\n","")
+
+  local file = io.open("/UOS/apps.lua","w") 
+  file:write(dfile)
   file:close()
-  local textOriginal = getFromGitHub(applications[i].url,"/tmp/recovery.tmp")
-    if text == textOriginal then
-     print(applications[i].path .. "correct")
-    else
-     print("Downloading " .. applications[i].path)
-     getFromGitHub(applications[i].url,applications[i].path)
-   end
-    else
-      print("Downloading " .. applications[i].path)
-      getFromGitHub(applications[i].url,applications[i].path)
-    end
+  applications = dofile("/UOS/apps.lua")
+  local result = "y"
+  io.write("\n")
+    if not result or result == "" or result:sub(1, 1):lower() == "y" then
+      for i = 1, #applications do
+        print("Check " .. applications[i].path)
+        local size = fs.size(applications[i].path)
+          if fs.exists(applications[i].path) == true then
+            local file = io.open(applications[i].path,"r")
+            local text = file:read(size+1)
+            file:close()
+            local textOriginal = getFromGitHub(applications[i].url,"/tmp/recovery.tmp")
+            if text == textOriginal then
+              print(applications[i].path .. " true")
+            else
+              print("Downloading " .. applications[i].path)
+              getFromGitHub(applications[i].url,applications[i].path)
+            end
+          else
+            print("Downloading " .. applications[i].path)
+              getFromGitHub(applications[i].url,applications[i].path)
+          end
+        end
+      end
+    term.clear()
+    firstMenu()
   end
-end
-end
 
 write("\nInstallation completed!\n")
 
