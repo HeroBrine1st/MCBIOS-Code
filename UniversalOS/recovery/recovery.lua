66e3a92ff981648e26670ce933a7e29364e3021a
HeroBrine1st <herobrine1.videos@gmail.com>
2016-12-02 21:17:59 +0300

    Update recovery.lua

--- UniversalOS/recovery/recovery.lua
+++ UniversalOS/recovery/recovery.lua
@@ -3,12 +3,13 @@ local component = require("component")
 local computer = require("computer")
 local fs = require("filesystem")
 local internet = require("internet")
-local serialization = require("serialization")
 local shell = require("shell")
+local GitHubInstallerUrl = "https://raw.githubusercontent.com/HeroBrine1st/OpenComputers/master/UniversalOS/UOS/install.lua"
+local GitHubApplicationsUrl = "https://raw.githubusercontent.com/HeroBrine1st/OpenComputers/master/UniversalOS/UOS/applications.txt"
+local gpu =  component.gpu
 local event = require("event")
+local package = require("package")
 local term = require("term")
-local GitHubUrl = "https://raw.githubusercontent.com/"
-
 local function internetRequest(url)
   local success, response = pcall(component.internet.request, url)
     if success then
@@ -43,41 +44,85 @@ local function getFromGitHub(url,filepath)
    file:write(reason)
    file:close()
    return success, reason
+ else
+   io.stderr("Can't download " .. url .. "\n")
  end
 end
+local function dI()
+_G.eventInterruptBackup = package.loaded.event.shouldInterrupt 
+_G.eventSoftInterruptBackup = package.loaded.event.shouldSoftInterrupt 
+package.loaded.event.shouldInterrupt = function () return false end
+package.loaded.event.shouldSoftInterrupt = function () return false end
+end
 
-write("Downloading file list    ")
+
+local function eI()
+if _G.eventInterruptBackup then
+package.loaded.event.shouldInterrupt = _G.eventInterruptBackup 
+package.loaded.event.shouldSoftInterrupt = _G.eventSoftInterruptBackup
+end
+end
+local w,h = gpu.getResolution()
+local function firstMenu()
+gpu.setBackground(0xCCCCCC)
+gpu.fill(1,1,w,h," ")
+gpu.setBackground(0xFFFFFF)
+gpu.setForeground(0x000000)
+gpu.fill(1,1,w,1," ")
+gpu.set(1,1,"Reinstall system")
+gpu.fill(1,3,w,3," ")
+gpu.set(1,3,"Exit")
+gpu.setForeground(0xFFFFFF)
+gpu.setBackground(0xCCCCCC)
+gpu.set(1,2,"Repair/update system")
+gpu.fill(1,4,w,5," ")
+end
+
+local w,h = gpu.getResolution()
+local function status(msg)
+end
+
+gpu.setBackground(0xCCCCCC)
+dI()
+gpu.fill(1,1,w,h," ")
+
+--status("Booting recovery")
+firstMenu()
+
+while true do
+local touch = {event.pull("touch")}
+if touch[4]==1 then
+gpu.setBackground(0x000000)
+term.clear()
+write("Downloading file list...    ")
+
 local success, reason = getFromGitHub("https://raw.githubusercontent.com/HeroBrine1st/OpenComputers/master/UniversalOS/UOS/applications.txt","/UOS/applications.txt")
 
-local dfile
-local applications
-if success == true then
+  
+  local applications
+  if success == true then
 dfile = "return " .. string.gsub(reason,"\n","")
 write("Success\n\n")
 else
 error("Error. Reason: " .. reason)
 end
-reason = nil
-success = nil
-local file = io.open("/UOS/apps.lua","w")
-file:write(dfile)
-file:close()
-applications = dofile("/UOS/apps.lua")
 
-for i = 1, #applications do
-write("Downloading \"" .. applications[i].name .. "\"...     ")
-local success, reason = getFromGitHub(applications[i].url, applications[i].path)
-if success == true then
-io.write("Success\n")
+  local file = io.open("/UOS/apps.lua","w") 
+  file:write(dfile)
+  file:close()
+  applications = dofile("/UOS/apps.lua")
+firstMenu()
 end
-if success == false then
-io.stderr("Error. Reason: " .. reason)
+if touch[4]==3 then
+gpu.setBackground(0x000000)
+term.clear()
+eI()
+break
 end
-end
-
-write("\nCheck downloaded files...\n\n")
-
-write("Downloading file list...    ")
+if touch[4]==2 then
+  gpu.setBackground(0x000000)
+  term.clear()
+  write("Downloading file list...    ")
 
 local success, reason = getFromGitHub("https://raw.githubusercontent.com/HeroBrine1st/OpenComputers/master/UniversalOS/UOS/applications.txt","/UOS/applications.txt")
 
@@ -104,18 +149,18 @@ end
             local file = io.open(applications[i].path,"r")
             local text = file:read(size+1)
             file:close()
-            local success, textOriginal = getFromGitHub(applications[i].url,"/tmp/systemchecker.tmp")
+            local success, textOriginal = getFromGitHub(applications[i].url,"/tmp/recovery.tmp")
             if text == textOriginal then
               print(applications[i].path .. " true")
             else
               write("Downloading " .. applications[i].path .. "    ")
               local success, reason = getFromGitHub(applications[i].url,applications[i].path)
               if success == true then
-				io.write("Success")
-				end
-				if success == false then
-				io.stderr("error. Reason: " .. reason)
-				end
+        io.write("Success")
+        end
+        if success == false then
+        io.stderr("error. Reason: " .. reason)
+        end
             end
           else
             write("Downloading " .. applications[i].path .. "    ")
@@ -129,15 +174,7 @@ end
           end
         end
       end
-
-write("\nInstallation completed!\n")
-
-write("\nReboot now? [Y/n] ")
-  local result = read()
-  if not result or result == "" or result:sub(1, 1):lower() == "y" then
-    write("\nRebooting now!\n")
-    computer.shutdown(true)
-  end
-
 term.clear()
-write("Returning to shell\n")
+firstMenu()
+end  
+end
