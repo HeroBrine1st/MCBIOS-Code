8c8950dc03a8200b17685265f78bb6a12a78b425
HeroBrine1st <herobrine1.videos@gmail.com>
2016-12-21 16:22:58 +0300

    Update mics.lua

--- UniversalOS/init.d/mics.lua
+++ UniversalOS/init.d/mics.lua
@@ -26,30 +26,15 @@ local w,h = gpu.getResolution()
 while true do
 local touch = {event.pull("touch")}
 if touch[4]==2 then
-buffer.start()
-
 local str ="Booting internal system"
-local lenght = (unicode.len(str)/2)
-local x1 = w/2-lenght-5
-local y1 = h/2-5
-local w1 = lenght*2+5
-local h1 = 10
-buffer.square(x1,y1,w1,h1,0xF3F3F3,0xF3F3F3," ",100)
 gpu.setBackground(0x000000)
 gpu.set(w/2-lenght,h/2,str)
 os.sleep(0.5)
 break
 end
 if touch[4]==3 then
-buffer.start()
 local str ="Booting recovery"
 local lenght = (unicode.len(str)/2)
-local x1 = w/2-lenght-5
-local y1 = h/2-5
-local w1 = lenght*2+5
-local h1 = 10
-buffer.square(x1,y1,w1,h1,0xF3F3F3,0xF3F3F3," ",100)
-gpu.setBackground(0x000000)
 gpu.set(w/2-lenght,h/2,str)
 os.sleep(0.5)
 dofile("/recovery/recovery.lua")
