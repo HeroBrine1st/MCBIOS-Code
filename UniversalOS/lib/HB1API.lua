d2ff90f11b6a49aa27dcd254f30382eca5fb94fb
HeroBrine1st <herobrine1.videos@gmail.com>
2016-12-30 14:02:29 +0300

    Create HB1API.lua

---
+++ UniversalOS/lib/HB1API.lua
@@ -0,0 +1,37 @@
+local component = require("component")
+local gpu = component.gpu
+
+local hbfirst = {}
+
+
+function hbfirst.setPixel(x,y,color)
+local oldColor = gpu.setBackground(color)
+gpu.set(x,y," ")
+gpu.setBackground(oldColor)
+end
+
+function hbfirst.drawButton(x,y,w,h,text,buttonColor,textColor)
+local oldBackground = gpu.setBackground(buttonColor)
+local oldForeground = gpu.setForeground(textColor)
+gpu.fill(x,y,w,h," ")
+local x2 = w/2-string.len(text)
+local y2 = h/2
+local tx = x+x2
+local ty = x+y2-1
+gpu.set(tx,ty,text)
+gpu.setBackground(oldBackground)
+gpu.setForeground(oldForeground)
+os.sleep(0.1)
+end
+
+function hbfirst.clickedAtArea(x,y,x2,y2,touchX,touchY)
+if (touchX >= x) and (touchX <= x2) and (touchY >= y) and (touchY <= y2) then 
+return true 
+end 
+return false
+end
+
+
+
+
+return hbfirst
