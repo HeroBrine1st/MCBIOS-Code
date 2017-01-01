9427b8a84beab70681306a7a261943b5c430960e
HeroBrine1st <herobrine1.videos@gmail.com>
2017-01-01 20:17:52 +0300

    Update recovery.init.lua

--- UniversalOS/recovery/recovery.init.lua
+++ UniversalOS/recovery/recovery.init.lua
@@ -183,23 +183,24 @@ do
   
 gpu = require("component").gpu
 local function err(msg)
-local Math = math
-gpu.setResolution(40,12)
-gpu.setBackground(0x0000FF)
-local w,h = gpu.getResolution()
-gpu.fill(1,1,w,h," ")
-gpu.set(8,3,":(")
+  local Math = math
+local gp = component.proxy(component.list("gpu")())
+gp.setResolution(40,12)
+gp.setBackground(0x0000FF)
+local w,h = gp.getResolution()
+gp.fill(1,1,w,h," ")
+gp.set(8,3,":(")
 local str = "Your PC ran into a problem and needs to restart. We're just collecting some error info, and then we'll restart for you."
-gpu.set(8,4,string.sub(str,1,28))
-gpu.set(8,5,string.sub(str,29,56))
-gpu.set(8,6,string.sub(str,57,84))
-gpu.set(8,7,string.sub(str,85,112))
-gpu.set(8,8,string.sub(str,113,140))
-gpu.set(8,10,"Error code: " .. string.sub(msg,1,16))
-gpu.set(8,11,string.sub(msg,17))
+gp.set(8,4,string.sub(str,1,27))
+gp.set(8,5,string.sub(str,28,55))
+gp.set(8,6,string.sub(str,56,82))
+gp.set(8,7,string.sub(str,83,110))
+gp.set(8,8,string.sub(str,112,138))
+gp.set(8,10,"Error code: " .. string.sub(msg,1,16))
+gp.set(8,11,string.sub(msg,17))
 
 while true do
-require("computer").pullSignal()
+computer.pullSignal()
 end
 end
 
