792b405e8bae15ffb87e4b6eac73f1db3dc51cbe
HeroBrine1st <herobrine1.videos@gmail.com>
2017-01-15 16:48:06 +0300

    Update autorun.lua

--- TabletOS/autorun.lua
+++ TabletOS/autorun.lua
@@ -309,6 +309,8 @@ drawBar()
 
 function apps.fileManager()
 local fileManagerPixels = ecs.rememberOldPixels(1,1,80,25)
+gpu.setBackground(0x610B5E)
+gpu.setForeground(0xFFFFFF)
 gpu.set(1,1,languagePackages[language].fileManager)
 local form = zygote.addForm()
 form.left=1
