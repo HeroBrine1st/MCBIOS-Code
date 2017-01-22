6867d273a2f886a9260734ad6b565deae8a1cf88
HeroBrine1st <herobrine1.videos@gmail.com>
2017-01-22 11:50:12 +0300

    Create shell.lua

---
+++ TabletOS/apps/shell.lua
@@ -0,0 +1,36 @@
+local shell = require("shell")
+local term = require("term")
+local ecs = require("ECSAPI")
+term.clear()
+local success, oldShellPixels = false, nil
+while success == false do
+success, oldShellPixels = pcall(ecs.rememberOldPixels,1,1,80,25)
+if success == false then io.stderr:write(oldShellPixels) end
+end
+term.clear()
+local function execute(command)
+	local success, reason = shell.execute(command)
+	if not success then
+		reason = debug.traceback(reason)
+	end
+	return success, reason
+end
+
+
+while true do
+local path = shell.getWorkingDirectory()
+io.stderr:write("Root@TabletOS".. path .. "#")
+local result
+while true do
+result = io.read()
+if result ~= nil and result ~= "" then
+break
+end
+end
+io.write("\n")
+local success, reason = execute(result)
+if not success then
+io.stderr:write(reason)
+io.write("\n")
+end
+end
