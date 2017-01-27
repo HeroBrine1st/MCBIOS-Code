f951673b24bc5b9fb5fa08b9b6d2a3ac9f07a73a
HeroBrine1st <herobrine1.videos@gmail.com>
2017-01-27 21:34:40 +0300

    Update init.lua

--- TabletOS/init.lua
+++ TabletOS/init.lua
@@ -2,13 +2,11 @@ local function loadFile(file)
 local filesystem = component.proxy(computer.getBootAddress())
 local handle, reason = filesystem.open(file,"r")
 if not handle then error(reason) end
-local readed = ""
-repeat
-	local data, reason = filesystem.read(handle,math.huge)
+
+	local data, reason = filesystem.read(handle,40000000)
 	if not data and reason then error(reason) end
-	readed = readed .. data
-until not data
-return load(readed)
+
+return load(data)
 end
 
 local success, reason = pcall(loadFile("/boot.lua"))
