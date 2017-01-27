09f9f8c2097d0c30232dfb2748f833c9e891be70
HeroBrine1st <herobrine1.videos@gmail.com>
2017-01-27 21:39:19 +0300

    Update init.lua

--- TabletOS/init.lua
+++ TabletOS/init.lua
@@ -1,13 +1,43 @@
-local function loadFile(file)
-local filesystem = component.proxy(computer.getBootAddress())
-local handle, reason = filesystem.open(file,"r")
-if not handle then error(reason) end
+  local rom = {}
+  function rom.invoke(method, ...)
+    return component.invoke(computer.getBootAddress(), method, ...)
+  end
+  function rom.open(file) return rom.invoke("open", file) end
+  function rom.read(handle) return rom.invoke("read", handle, math.huge) end
+  function rom.close(handle) return rom.invoke("close", handle) end
+  function rom.inits() return ipairs(rom.invoke("list", "boot")) end
+  function rom.isDirectory(path) return rom.invoke("isDirectory", path) end
 
-	local data, reason = filesystem.read(handle,40000000)
-	if not data and reason then error(reason) end
+  local function loadfile(file)
+    local handle, reason = rom.open(file)
+    if not handle then
+      error(reason)
+    end
+    local buffer = ""
+    repeat
+      local data, reason = rom.read(handle)
+      if not data and reason then
+        error(reason)
+      end
+      buffer = buffer .. (data or "")
+    until not data
+    rom.close(handle)
+    return load(buffer, "=" .. file)
+  end
 
-return load(data)
-end
+  local function dofile(file)
+    local program, reason = loadfile(file)
+    if program then
+      local result = table.pack(pcall(program))
+      if result[1] then
+        return table.unpack(result, 2, result.n)
+      else
+        error(result[2])
+      end
+    else
+      error(reason)
+    end
+  end
 
-local success, reason = pcall(loadFile("/boot.lua"))
+local success, reason = pcall(loadfile("/boot.lua"))
 if not success then error(reason) end
