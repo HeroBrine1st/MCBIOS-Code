5901705f8155e8d7bf1e2d259859809115cb4d4f
HeroBrine1st <herobrine1.videos@gmail.com>
2017-02-19 20:38:40 +0300

    Update fastboot.lua

--- TabletOS/fastboot.lua
+++ TabletOS/fastboot.lua
@@ -1,6 +1,20 @@
 --Интерфейс к fastboot планшета. для включения на планшете - включите и сразу же держите любую кнопку на клавиатуре
 
-local a=require("component")local b=require("computer")local c=a.modem;local d=require("filesystem")if not d.exists("/lib/forms.lua")then os.execute("pastebin get iKzRve2g lib/forms.lua")end;local e=require("forms")local f,g=a.gpu.getResolution()io.write("Enter port:")local h=io.read()h=tonumber(h)local i=e.addForm()local j=i:addButton(1,1,"Repair/update system",function()c.broadcast(h,[[local function internetRequest(url)
+local component = require("component")
+local computer = require("computer")
+local modem = component.modem
+local fs = require("filesystem")
+if not fs.exists("/lib/forms.lua") then os.execute("pastebin get iKzRve2g lib/forms.lua") end
+local forms = require("forms")
+
+local w, h  = component.gpu.getResolution()
+io.write("Enter port:")
+local port = io.read()
+port = tonumber(port)
+local form1 = forms.addForm()
+
+local forceUpdate = form1:addButton(1,1,"Repair/update system",function()
+modem.broadcast(port,[[local function internetRequest(url)
   local success, response = pcall(component.proxy(component.list("internet")()).request, url)
   if success then
     local responseData = ""
@@ -52,7 +66,15 @@ local downloads = load("return" .. string,"=filelistloader")()
 
 for i = 1, #downloads do
 getFile(downloads[i].url,downloads[i].path)
-end]])end)j.W=f;local k=i:addButton(1,2,"Uninstall OS",function()c.broadcast(h,[[local function internetRequest(url)
+end]])
+end)
+
+
+forceUpdate.W = w
+
+
+local uninstallButton = form1:addButton(1,2,"Uninstall OS",function()
+	modem.broadcast(port,[[local function internetRequest(url)
   local success, response = pcall(component.proxy(component.list("internet")()).request, url)
   if success then
     local responseData = ""
@@ -110,4 +132,17 @@ end
 
 getFile("https://raw.githubusercontent.com/HeroBrine1st/OpenComputers/master/TabletOS/initBackup.lua","init.lua")
 
-]])end)k.W=f;local l=i:addButton(1,3,"Reboot",function()c.broadcast(h,[[computer.shutdown(true)]])end)l.W=f;local m=i:addButton(1,3,"Shutdown",function()c.broadcast(h,[[computer.shutdown()]])end)m.W=f;local n=i:addButton(1,g,"Exit",function()error("No error! This error - exit program")end)n.W=f;e.run(i)
+]])
+end)
+
+
+uninstallButton.W = w
+
+local rebootButton = form1:addButton(1,3,"Reboot",function() modem.broadcast(port,[[computer.shutdown(true)]]) end)
+rebootButton.W = w
+local rebootButton1 = form1:addButton(1,4,"Shutdown",function() modem.broadcast(port,[[computer.shutdown()]]) end)
+rebootButton1.W = w
+
+local exitButton = form1:addButton(1,h,"Exit",function() error("No error! This error - exit program") end)
+exitButton.W = w
+forms.run(form1)
