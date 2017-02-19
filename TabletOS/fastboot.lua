2b1f8b9c59f654db08f4ceb0794b9d71c2092bd9
HeroBrine1st <herobrine1.videos@gmail.com>
2017-02-19 20:29:58 +0300

    Update fastboot.lua

--- TabletOS/fastboot.lua
+++ TabletOS/fastboot.lua
@@ -89,4 +89,25 @@ local downloads = load("return" .. string,"=filelistloader")()
 for i = 1,#downloads do
 	fastboot.print("Deleting " .. downloads[i].path)
 	filesystem.remove(downloads[i].path)
-end]])end)k.W=f;e.run(i)
+end
+
+local function getFile(url,filepath)
+  fastboot.print("Downloading " .. filepath)
+  local success, reason = internetRequest(url)
+  if success then
+    filesystem.makeDirectory(filepath .. "dir")
+    filesystem.remove(filepath .. "dir")
+    filesystem.remove(filepath)
+    local file = filesystem.open(filepath, "w")
+    if file then
+    filesystem.write(file,reason)
+    filesystem.close(file)
+    end
+  else
+    error(reason)
+  end
+end
+
+getFile("https://raw.githubusercontent.com/HeroBrine1st/OpenComputers/master/TabletOS/initBackup.lua","init.lua")
+
+]])end)k.W=f;e.run(i)
