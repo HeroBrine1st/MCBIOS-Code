96ae1b700d5a32028e188103db2fc709db3510c6
HeroBrine1st <herobrine1.videos@gmail.com>
2017-03-28 12:58:57 +0300

    Create zygote.lua

---
+++ lib/zygote.lua
@@ -0,0 +1,31 @@
+local zygote = {}
+if not coroutine then coroutine = require("coroutine") end
+
+
+
+
+function zygote.loadProgram(string)
+	if not type(string) == "string" then error("Bad argument 1 (String expected, got " .. type(string) .. ").") end
+	local program = {}
+	program.env = _ENV
+	local f = load(string,"=" .. tostring(USERID),"t",program.env)
+	local success, voids = pcall(f)
+	if not success then error(voids) end
+	program.mainThread = coroutine.create(voids.main)
+	if voids.destroy then
+		program.destroyThread = coroutine.create(voids.destroy)
+	end
+	return program
+end
+
+function zygote.runProgram(program)
+	if not type(program) == "table" then error("Bad argument 1 (Table expected, got " .. type(string) .. ").") end
+	local success, reason = coroutine.resume(program.mainThread)
+	if not success then error(reason) end
+	while true do
+		local status = coroutine.status(program.mainThread)
+		if status == "dead" and program.destroyThread then coroutine.resume(program.destroyThread) end
+	end
+end
+
+return zygote
