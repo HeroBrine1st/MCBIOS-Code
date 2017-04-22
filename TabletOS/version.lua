e4e92ba8eded92c753beee2477badb6cad64dbe6
HeroBrine1st <herobrine1.videos@gmail.com>
2017-04-22 17:20:06 +0300

    Update version.lua

--- TabletOS/version.lua
+++ TabletOS/version.lua
@@ -1,9 +1,7 @@
 return {
 	version = "0.1.2 Alpha-relelase",
 	changelog = {
-	ru = "Добавлен системный интерфейс OSAPI\n
-		Добавлена система обновлений",
-	eu = "Add system interface OSAPI\n
-		Add system updates",
+	ru = "Добавлен системный интерфейс OSAPI\nДобавлена система обновлений",
+	eu = "Add system interface OSAPI\nAdd system updates",
 	}
 }
