05d7dfc13f62939747be7d7d3a33ca7476293b5d
HeroBrine1st <herobrine1.videos@gmail.com>
2017-04-22 21:35:10 +0300

    Create appsLauncher.lua

---
+++ TabletOS/apps/appsLauncher.lua
@@ -0,0 +1,38 @@
+local pm = require("pm")
+local shell = require("shell")
+local zygote = require("zygote")
+local fs = require("filesyste,")
+local event = require("event")
+local form = zegote.addForm()
+form.left = 1
+form.top = 2
+form.W = 80
+form.H = 23
+local list = form:addList(1,1,function(view)
+	local value = view.items[view.index]
+	local windowForm = zygote.addForm()
+windowForm.left = 30
+windowForm.top = 12-2
+windowForm.W = 20
+windowForm.H = 2
+
+windowButton1 = windowForm:addButton(1,1,"Execute",function()
+OSAPI.ignoreListeners()
+shell.execute(value)
+pcall(event.cancel,_G.timerID)
+pcall(event.cancel,timerID)
+OSAPI.init()
+form:setActive()
+end)
+windowButton2 = windowForm:addButton(1,2,"Uninstall",function()
+pm.uninstallApp(fs.name(value))
+form:setActive()
+end)
+zygote.run(windowForm)
+end)
+for _, dir in pairs(pm.listOfApps(true))
+	list:insert(fs.name(dir),dir)
+end
+for _, dir in pairs(pm.listOfApps(false))
+	list:insert(fs.name(dir),dir)
+end
