4811c0bd7c06c8d1b9366a26e9c49e2db91d1082
HeroBrine1st <herobrine1.videos@gmail.com>
2017-04-22 20:48:26 +0300

    Create pm.lua

---
+++ lib/pm.lua
@@ -0,0 +1,31 @@
+local fs = require("filesystem")
+local serialization = require("serialization")
+local packageManager = {}
+packageManager.systemAppsPath = "/apps/"
+packageManager.userAppsPath = "/data/apps/"
+fs.makeDirectory(packageManager.userAppsPath)
+fs.makeDirectory(packageManager.systemAppsPath)
+
+function packageManager.installApp(file,name)
+	local appPath = fs.concat(packageManager.userAppsPath,name)
+	fs.copy(file,appPath)
+	return appPath
+end
+
+function packageManager.uninstallApp(name)
+	local appPath = fs.concat(packageManager.userAppsPath,name)
+	assert(fs.remove(appPath))
+end
+
+function packageManager.listOfApps(system)
+	local appsDir = system and appsDir = packageManager.systemAppsPath or packageManager.userAppsPath
+	local list = {}
+	for file in fs.list(appsDir) do 
+		if not fs.isDirectory(fs.concat(appsDir,file)) then
+			table.insert(list,fs.concat(appsDir,file))
+		end
+	end
+	return list
+end
+
+return packageManager
