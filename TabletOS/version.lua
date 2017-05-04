53d956097b93035df96734fa18efe0ba33a8bd2a
HeroBrine1st <herobrine1.videos@gmail.com>
2017-05-04 22:27:21 +0300

    Update version.lua

--- TabletOS/version.lua
+++ TabletOS/version.lua
@@ -1,7 +1,7 @@
 return {
-	version = "0.2.1 Beta-prerelelase micropatch",
+	version = "0.2.1.2 Beta-prerelelase micropatch",
 	changelog = {
-	ru = "Исправлены баги, исправлено окно обновления, исправлен монитор онлайна",
-	eu = "Fixed bugs,fixed update window, fixed monitor online",
+	ru = "Исправлены баги (+1), исправлено окно обновления, исправлен монитор онлайна",
+	eu = "Fixed bugs (+1),fixed update window, fixed monitor online",
 	}
 }
