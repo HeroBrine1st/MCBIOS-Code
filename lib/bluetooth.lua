3ae00b242aac118dbf3ea7ff604dc90a8cd40311
HeroBrine1st <herobrine1.videos@gmail.com>
2017-10-14 15:21:28 +0300

    Update bluetooth.lua

--- lib/bluetooth.lua
+++ lib/bluetooth.lua
@@ -1,1 +1,121 @@
-local a=require("computer")local b=require("event")local c=require("filesystem")local d=require("component")local e=d.modem;local f={port=61593,state=false,opened=false,name="TabletOS",devices={}}f.listener=function(...)local g={...}if not g[4]==f.port then return end;local h=g[6]if h=="PING"and f.opened then e.send(g[3],f.port,"PONG",f.name)end;if h=="PONG"then a.pushSignal("BLUETOOTH","PONG",g[3],g[7])end end;function f.on()f.state=true;e.open(f.port)b.listen("modem_message",f.listener)end;function f.off()f.state=false;e.close(f.port)b.ignore("modem_message",f.listener)end;function f.open()f.opened=true end;function f.close()f.opened=false end;function f.sendFile(i,j,k)k=k or function()end;if not c.exists(i)or c.isDirectory(i)then return false end;local l=c.name(i)e.send(j,f.port,"SEND_REQUEST",l,f.name)local m=false;local n=false;while not n do local o,o,p,o,o,q=b.pull("modem_message")if q=="REQUEST_ALLOWED"and p==j then n=true;m=true elseif q=="REQUEST_DENIED"and p==j then n=true end end;if m then totalSize=0;e.send(j,f.port,"SEND_START",c.size(i))local r=io.open(i,"r")repeat local s=r:read(128)if s then totalSize=totalSize+128;e.send(j,f.port,"FILE_CHUNK",s)k(size,totalSize)os.sleep(0.1)end until not s;r:close()return true end end;function f.receiveFile(t,u,v)t=t or function()end;u=u or function()end;v=v or function()return true end;while true do local g={b.pull()}if g[6]=="SEND_REQUEST"then local q=v(g[7],g[8])e.send(g[3],f.port,q and"REQUEST_ALLOWED"or"REQUEST_DENIED")if q then while true do local o,o,w,o,o,x,size=b.pull("modem_message")if x=="SEND_START"and w==g[3]then c.remove("/bluetooth/"..g[7])c.makeDirectory("/bluetooth/")local r=io.open("/bluetooth/"..g[7],"w")local totalSize=0;while true do if totalSize==size then r:close()return true end;local y={b.pull(10,"modem_message")}if#y<3 then return false end;if y[3]==g[3]and y[6]=="FILE_CHUNK"then r:write(y[7])totalSize=totalSize+#y[7]u(size,totalSize)end end end end end end end end;function f.scan()if not f.state then f.on()end;e.broadcast(f.port,"PING")local z={}while true do local g={b.pull(5,"modem_message")}if#g<3 then break end;if g[6]=="PONG"then table.insert(z,{address=g[3],name=g[7]})end end;f.devices=z;return z end;return f
+local computer = require("computer")
+local event = require("event")
+local fs = require("filesystem")
+local component = require("component")
+local modem = component.modem
+local bluetooth = {
+	port = 61593,
+	state = false,
+	opened = false,
+	name = "TabletOS",
+	devices = {},	
+}
+
+local listener = function(...) 
+	local signal = {...}
+	if not signal[4] == bluetooth.port then return end 
+	local reason = signal[6] 
+	
+	if reason == "PING" and bluetooth.opened then 
+		modem.send(signal[3],bluetooth.port,"PONG",bluetooth.name)
+	end
+	if reason == "PONG" then
+		computer.pushSignal("BLUETOOTH","PONG",signal[3],signal[7])
+	end
+end
+
+function bluetooth.on()
+	bluetooth.state = true
+	modem.open(bluetooth.port)
+	event.listen("modem_message",listener)
+end
+
+function bluetooth.off()
+	bluetooth.state = false
+	modem.close(bluetooth.port)
+	event.ignore("modem_message",listener)
+end
+
+function bluetooth.open() bluetooth.opened = true end
+function bluetooth.close() bluetooth.opened = false end
+
+function bluetooth.sendFile(path,deviceAddress,dialogSending)
+	dialogSending = dialogSending or function() end
+	if not fs.exists(path) or fs.isDirectory(path) then return false end
+	local name = fs.name(path)
+	modem.send(deviceAddress,bluetooth.port,"SEND_REQUEST",name,bluetooth.name)
+	local send = false
+	local requestSuccess = false
+	while not requestSuccess do
+		local _,_,senderAddress,_,_,answer = event.pull("modem_message")
+		if answer == "REQUEST_ALLOWED" and senderAddress == deviceAddress then requestSuccess = true send = true elseif answer == "REQUEST_DENIED" and senderAddress == deviceAddress then requestSuccess = true end  
+	end
+	if send then
+		totalSize = 0
+		modem.send(deviceAddress,bluetooth.port,"SEND_START",fs.size(path))
+		local f = io.open(path,"r")
+		repeat
+			local data = f:read(128)
+			if data then
+				totalSize = totalSize + 128
+				modem.send(deviceAddress,bluetooth.port,"FILE_CHUNK",data)
+				dialogSending(size,totalSize)
+				os.sleep(0.1)
+			end
+		until not data
+		f:close()
+		return true
+	end
+end
+
+function bluetooth.receiveFile(dialogWait,dialogReceiving,dialogAnswer)
+	dialogWait = dialogWait or function() end
+	dialogReceiving = dialogReceiving or function() end
+	dialogAnswer = dialogAnswer or function() return true end
+	while true do
+		local signal = {event.pull()}
+		dialogWait()
+		if signal[6] == "SEND_REQUEST" then
+			local answer = dialogAnswer(signal[7],signal[8])
+			modem.send(signal[3],bluetooth.port,answer and "REQUEST_ALLOWED" or "REQUEST_DENIED")
+			if answer then
+				while true do
+					local _,_,address,_,_,status,size = event.pull("modem_message")
+					if status == "SEND_START" and address == signal[3] then
+						fs.remove("/bluetooth/" .. signal[7])
+						fs.makeDirectory("/bluetooth/") 
+						local f = io.open("/bluetooth/" .. signal[7],"w")
+						local totalSize = 0
+						while true do
+							if totalSize == size then f:close() return true end
+							local signal2 = {event.pull(10,"modem_message")}
+							if #signal2 < 3 then return false end
+							if signal2[3] == signal[3] and signal2[6] == "FILE_CHUNK" then
+								f:write(signal2[7])
+								totalSize = totalSize + #signal2[7]
+								dialogReceiving(size,totalSize)
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+end
+
+function bluetooth.scan()
+	if not bluetooth.state then bluetooth.on() end
+	modem.broadcast(bluetooth.port,"PING")
+	local devices = {}
+	while true do
+		local signal = {event.pull(1,"modem_message")}
+		if #signal < 3 then break end
+		if signal[6] == "PONG" then		
+			table.insert(devices,{address=signal[3],name=signal[7]})
+		end
+	end
+	bluetooth.devices = devices
+	return devices
+end
+
+return bluetooth
